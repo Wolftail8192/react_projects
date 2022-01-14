6924514e273216553146f898acfea5b98b8ec129
Wolftail8192 <4420299@gmail.com>
2022-01-14 06:20:38 +0200

    cw_les1

--- src/Components/Flight.js
+++ src/Components/Flight.js
@@ -1,8 +1,14 @@
+import './Flight.css';
 export default function Flight ({item:value}) {
     return(
-        <div>
-            {value.mission_name} - {value.launch_year}
-            <img src={value.links.mission_patch} alt="mission_patch"/>
+        <div className ='ff'>
+            <div className = 'left'>
+
+                {value.mission_name} - {value.launch_year}
+            </div>
+            <div className = 'right'>
+                <img src={value.links.mission_patch} alt="mission_patch"/>
+            </div>
         </div>
     )
 }
